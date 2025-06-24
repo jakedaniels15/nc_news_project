edc14affcc0ba07483c872747f764d21768ccff2
jakedaniels15 <jake.daniels15@hotmail.co.uk>
2025-06-24 12:00:18 +0100

    more articles added

--- listen.js
+++ listen.js
@@ -2,10 +2,6 @@ const app = require("./app")
 
 const {PORT = 9092} = process.env
 
-if (process.env.NODE_ENV === "production") {
-  require("./db/seeds/run-seed")();
-}
-
 app.listen(PORT, () => {
   console.log(`Server is listening on ${PORT}...`);
 });//
